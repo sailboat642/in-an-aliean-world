f811c5411f47faaa44ac4ed1af5c92dade6cc839
Nishith Agarwal <sailboat642@gmail.com>
2026-02-01 16:03:07 +0530

    adds player righ

--- Levels/StartScene/start_scene.gd
+++
@@ -1,8 +0,0 @@
-extends Node2D
-
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	var stream = AudioManager.play_music("mx_tutorial")
-	AudioManager.set_loop(stream, true)
-	
